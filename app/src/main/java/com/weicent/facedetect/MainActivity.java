348ac591dd3325c47f5b1386e3acfbf3b66e04c0
hzl520 <hzl.ok@qq.com>
2019-01-24 17:42:55 +0800

    基于openCV的人脸识别demo

---
+++ app/src/main/java/com/weicent/facedetect/MainActivity.java
@@ -0,0 +1,18 @@
+package com.weicent.facedetect;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+/**
+ * @author hzl520
+ * @date 2019-01-24
+ */
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
